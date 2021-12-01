fab095656183fb171121d168529e1e2c32cb8fc0
Markku Lammerz <markku.lammerz@gmail.com>
2021-12-01 21:37:54 +0100

    Day1 solution with performance test

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,89 @@
+import java.util.concurrent.ThreadLocalRandom
+
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun part1(depthData: List<Int>): Int {
+        var measurementIncreases: Int = 0
+        for (index in depthData.indices) {
+            if (index >= 1) {
+                val depthDifference = depthData[index] - depthData[index - 1]
+                if (depthDifference > 0) {
+                    measurementIncreases = measurementIncreases.inc()
+                }
+            }
+        }
+        return measurementIncreases
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2Normal(depthData: List<Int>): Int {
+        fun calculateMeasurementSlidingWindow(startIndex: Int, depthData: List<Int>): Int {
+            return depthData[startIndex] + depthData[startIndex + 1] + depthData[startIndex + 2]
+        }
+
+        var measurementIncreases: Int = 0
+        for (index in depthData.indices) {
+            if (index + 3 <= depthData.size - 1) {
+                val firstSlidingWindowValue: Int = calculateMeasurementSlidingWindow(index, depthData)
+                val secondSlidingWindowValue: Int = calculateMeasurementSlidingWindow(index + 1, depthData)
+                val depthDifference: Int = secondSlidingWindowValue - firstSlidingWindowValue
+
+                if (depthDifference > 0) {
+                    measurementIncreases = measurementIncreases.inc()
+                }
+            }
+        }
+        return measurementIncreases
     }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    fun part2Short(depthData: List<Int>): Int {
+        return depthData.asSequence().windowed(3).map { it.sum() }.windowed(2).count { it[1] > it[0] }
+    }
 
-    val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+    fun part2Speedy(depthData: List<Int>): Int {
+        var measurementIncreases: Int = 0
+        for (index in depthData.indices) {
+            if (index + 3 >= depthData.size) {
+                break
+            }
+            if (depthData[index] < depthData[index + 3]) {
+                measurementIncreases++
+            }
+        }
+        return measurementIncreases
+    }
+
+    // test if implementation meets criteria from the description:
+    val testDepthData: List<Int> = readInput("Day01_test").map(String::toInt)
+    check(part1(testDepthData) == 7)
+    check(part2Normal(testDepthData) == 5)
+    check(part2Short(testDepthData) == 5)
+    check(part2Speedy(testDepthData) == 5)
+
+    // print results for 'real' data input:
+    val realDepthData: List<Int> = readInput("Day01").map(String::toInt)
+    runFuncWithMeasurment(
+        listOf(
+            Pair("step1_normal", { part1(realDepthData) }),
+            Pair("step2_normal", { part2Normal(realDepthData) }),
+            Pair("step2_short", { part2Short(realDepthData) }),
+            Pair("step2_speedy", { part2Speedy(realDepthData) })
+        )
+    )
+
+    println("-----------------------------------------------")
+    println("---------- generating random terrain ----------")
+    var lastDepth = 173
+    val depths = mutableListOf<Int>()
+    for (index in 0..100_000_000) {
+        lastDepth += ThreadLocalRandom.current().nextInt(-15, 20)
+        depths.add(lastDepth)
+    }
+    println("-----------------------------------------------")
+    runFuncWithMeasurment(
+        listOf(
+            Pair("step1_normal", { part1(depths) }),
+            Pair("step2_normal", { part2Normal(depths) }),
+            Pair("step2_short", { part2Short(depths) }),
+            Pair("step2_speedy", { part2Speedy(depths) })
+        )
+    )
 }
--- src/Utils.kt
+++ src/Utils.kt
@@ -1,6 +1,8 @@
 import java.io.File
 import java.math.BigInteger
 import java.security.MessageDigest
+import java.time.Duration
+import kotlin.system.measureNanoTime
 
 /**
  * Reads lines from the given input txt file.
@@ -11,3 +13,22 @@ fun readInput(name: String) = File("src", "$name.txt").readLines()
  * Converts string to md5 hash.
  */
 fun String.md5(): String = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray())).toString(16)
+
+
+fun runFuncWithMeasurment(blocks: List<Pair<String, () -> Int>>) {
+    val times = mutableListOf<Pair<String, Long>>()
+    for (block in blocks) {
+        val label = block.first
+        var value: Int
+        val time = measureNanoTime {
+            value = block.second()
+        }
+        println("Value for $label: $value")
+        times.add(Pair(label, time))
+    }
+    println("------------------------------------")
+    times.forEach { println("${it.first}: ${Duration.ofNanos(it.second)}") }
+    val min = times.minByOrNull { it.second }
+    println("------------------------------------")
+    println("fastest was: ${min?.first} in ${min?.second?.let { Duration.ofNanos(it) }}")
+}
