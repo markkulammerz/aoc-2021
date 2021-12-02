fdfcadda6ef38c04b1e1533b73362864c94e6370
Markku Lammerz <markku.lammerz@gmail.com>
2021-12-02 18:07:03 +0100

    Day2 Part 1 long version added

--- src/day2/Day02.kt
+++ src/day2/Day02.kt
@@ -1,23 +1,37 @@
 fun main() {
-    fun part1(depthData: List<String>): Int {
-        return 0
+    fun part1(movements: List<String>): Int {
+        var forwardPosition = 0;
+        var depthPosition = 0;
+        for (movement in movements) {
+            val splittedMovement = movement.split(' ');
+            when (splittedMovement[0]) {
+                "forward" -> forwardPosition = forwardPosition.plus(splittedMovement[1].toInt())
+                "up" -> depthPosition = depthPosition.minus(splittedMovement[1].toInt())
+                "down" -> depthPosition = depthPosition.plus(splittedMovement[1].toInt())
+                else -> println("cant recognize token")
+            }
+        }
+
+        return forwardPosition * depthPosition;
     }
 
-    fun part2(depthData: List<String>): Int {
+    fun part2(movements: List<String>): Int {
         return 0
     }
 
 
     // test if implementation meets criteria from the description:
-    val testMovements: List<String> = readInput("day2/Day01_test")
+    val testMovements: List<String> = readInput("day2/input-day2_test")
     check(part1(testMovements) == 150)
+    // check(part2(testMovements) == 900)
 
 
     // print results for 'real' data input:
-    val movements: List<String> = readInput("day2/Day01")
+    val movements: List<String> = readInput("day2/input-day2")
     runFuncWithMeasurment(
         listOf(
-            Pair("step1_normal", { part1(movements) }),
+            Pair("step1", { part1(movements) }),
+            Pair("step2", { part2(movements) }),
         )
     )
 }
