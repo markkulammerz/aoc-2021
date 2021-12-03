838fd7a1a3c1dfd15d01aa1715775a08fdbcdca1
Simon <simon@bytemonkeys.de>
2021-12-03 21:32:17 +0100

    methode mit bitsets für part1

--- src/day3/Day03.kt
+++ src/day3/Day03.kt
@@ -3,6 +3,7 @@ package day3
 import readInput
 import runFuncWithMeasurement
 import toDecimalValue
+import java.util.*
 
 fun main() {
     fun reduceMultipleListBits(list1: List<Int>, list2: List<Int>): List<Int> {
@@ -19,6 +20,31 @@ fun main() {
         return gammaRate * epsilonRate
     }
 
+    fun part1sk(sensorData: List<String>): Int {
+        val referenceValue = sensorData.first()
+        // assume all have the same length
+        val bits = referenceValue.map { BitSet(sensorData.size) }
+        for (i in sensorData.indices) {
+            val dataLine = sensorData[i]
+            for (j in dataLine.indices) {
+                val bit = dataLine[j]
+                bits[j][i] = bit == '1'
+            }
+        }
+
+        val gamma = BitSet(referenceValue.length)
+        val epsilon = BitSet(referenceValue.length)
+        for (i in bits.indices) {
+            val bitset = bits[bits.size - 1 - i]
+            val trueBits = bitset.cardinality()
+            val falseBits = sensorData.size - bitset.cardinality()
+            gamma[i] = trueBits > falseBits
+            epsilon[i] = falseBits > trueBits
+        }
+
+        return (gamma.toLongArray()[0] * epsilon.toLongArray()[0]).toInt()
+    }
+
     fun part2(sensorData: List<String>): Int {
         return 0
     }
@@ -26,14 +52,16 @@ fun main() {
     // test if implementation meets criteria from the description:
     val sensorTestData: List<String> = readInput("day3/input-day3_test")
     check(part1(sensorTestData) == 198)
+    check(part1sk(sensorTestData) == 198)
     // check(part2(testMovements) == 900)
 
 
     // print results for 'real' data input:
-    val sensorData: List<String> = readInput("day3/input-day3")
+    val sensorData: List<String> = readInput("day3/input-day3-simon")
     runFuncWithMeasurement(
         listOf(
             Pair("step1", { part1(sensorData) }),
+            Pair("step1sk", { part1sk(sensorData) }),
             Pair("step2", { part2(sensorData) }),
         )
     )
