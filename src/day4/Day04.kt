2d508151bdaafb4ee896751cd7efba3a3a899f05
Markku Lammerz <markku.lammerz@gmail.com>
2021-12-04 21:16:04 +0100

    Day 4 - Part 2

--- src/day4/Day04.kt
+++ src/day4/Day04.kt
@@ -6,19 +6,49 @@ import runFuncWithMeasurement
 fun part1(bingoDate: List<String>): Int {
     val (calledNumbers, bingoBoards) = generateBingoBoards(bingoDate)
 
+    while (calledNumbers.isNotEmpty() && bingoBoards.any { !it.hasWon }) {
+        val calledNumber = calledNumbers.removeFirst()
+
+        for (board in bingoBoards) {
+            board.numberCalled(calledNumber)
+
+            if (board.hasWon) {
+                return board.score
+            }
+        }
+    }
+
     return 0
 }
 
 fun part2(bingoDate: List<String>): Int {
     val (calledNumbers, bingoBoards) = generateBingoBoards(bingoDate)
 
+    while (calledNumbers.isNotEmpty() && !bingoBoards.isEmpty()) {
+        val calledNumber = calledNumbers.removeFirst()
+        val boardsToRemove = mutableListOf<BingoBoard>()
+
+        for (board in bingoBoards) {
+            board.numberCalled(calledNumber)
+
+            if (board.hasWon && bingoBoards.size > 1) {
+                boardsToRemove.add(board)
+            }
+        }
+        bingoBoards.removeAll(boardsToRemove)
+
+        if (bingoBoards.size == 1 && bingoBoards.first().hasWon) {
+            return bingoBoards.first().score
+        }
+    }
     return 0
 }
 
 fun main() {
     // test if implementation meets criteria from the description:
     val bingoDataTest: List<String> = readInput("day4/input-day4_test")
-    check(part1(bingoDataTest) == 6592)
+    check(part1(bingoDataTest) == 4512)
+    check(part2(bingoDataTest) == 1924)
 
     // print results for 'real' data input:
     val bingoData: List<String> = readInput("day4/input-day4")
@@ -57,16 +87,24 @@ class BingoBoard(boardData: List<List<Int>>) {
         get() {
             return state.any { check(it) } || state.indices.any { check(produceColumn(it)) }
         }
+    public val score: Int
+        get() {
+            return state.sumOf { arrayOfBingoSquares ->
+                arrayOfBingoSquares.filter { !it.isMarked }.sumOf { it.value }
+            } * lastNumberCalled
+        }
+    private var lastNumberCalled = 0
 
     init {
         state = Array(5) { row -> Array(5) { col -> BingoSquare(boardData[row][col], false) } }
     }
 
     fun numberCalled(number: Int) {
+        lastNumberCalled = number
         state.forEach {
             it.forEach { square ->
                 if (square.value == number) {
-                    square.isMarked = true
+                    square.mark()
                 }
             }
         }
@@ -81,7 +119,15 @@ class BingoBoard(boardData: List<List<Int>>) {
         return Array(5) { row -> state[row][index] }
     }
 
+    override fun toString(): String {
+        return "BingoBoard(state=${state.contentToString()}, hasWon=$hasWon)"
+    }
+
 
 }
 
-data class BingoSquare(val value: Int, var isMarked: Boolean)
+data class BingoSquare(val value: Int, var isMarked: Boolean) {
+    fun mark() {
+        this.isMarked = true
+    }
+}
